ee69c314eb82a7d3d9e61d64eeb739a9d792dcbb
Walter Michelin <wmichelin@gmail.com>
2015-01-18 12:29:03 -0500

    updated site
    everything

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,35 @@
+var contentHandler = function(){
+
+    var self = this;
+    self.init = function(){
+
+        self.openItem = $('.content:visible').data('id');
+
+        $('.navItem').on('click', function(){
+            var toOpen = $(this).data('id');
+            showSection(toOpen);
+        }); 
+    }
+
+    function showSection(toOpen){
+        if(toOpen !== self.openItem)
+        {   
+            $('.content[data-id=' + self.openItem + ']').slideToggle();
+            self.openItem = toOpen;
+            $('.content[data-id=' + self.openItem + ']').slideToggle();
+        }   
+
+    }
+    
+    return{
+        init: self.init,
+        showSection: showSection
+    }
+}();
+
+
+
+
 $(function(){
-	$('.navItem').hover(function(){
-		$(this).addClass('active');
-	}, function(){
-		$(this).removeClass('active');
-	});
-
-
-
+    contentHandler.init();
 })
